d0a310d1e697c4c172a5f9b7f797330b7e55635a
hudaa15 <80892376+hudaa15@users.noreply.github.com>
2021-03-18 19:28:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            taksi taxi = new taksi();
+
+            taxi.namadriver = " Hudaa ";
+            taxi.lagikerja = true;
+            taxi.penumpang = 10;
+
+            taxi.TaxiInfo();
+            taxi.angkutpenumpang();
+            taxi.turunpenumpang();
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taksi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi02
+{
+    class taksi
+    {
+        public string namadriver { get; set; }
+        public bool lagikerja { get; set; }
+        public int penumpang { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine(" Nama Supir      : {0} ", namadriver);
+            if (lagikerja == true) 
+            {
+                Console.WriteLine(" Sedang mengantar penumpang ", lagikerja);
+            }
+            else
+            {
+                Console.WriteLine(" Sedang tidak mengantar penumpang ", lagikerja);
+            }
+
+            Console.WriteLine(" Jumlah penumpang  : {0}\n ", penumpang);
+        }
+
+        public void angkutpenumpang()
+        {
+            Console.WriteLine(" {0} Sedang menjemput penumpang ", namadriver);
+        }
+
+        public void turunpenumpang()
+        {
+            Console.WriteLine(" {0} Selesai menjemput penumpang\n ", namadriver);
+        }
+
+    }
+
+
+}
